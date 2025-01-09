b1ef8e315bb4b4e01c28c14d91bc3a5bce8bb15e
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-01-10 02:25:05 +0700

    Initial commit: Completed Rust Registration Q1 2025 tutorial

---
+++ src/programs/Turbin3_prereq.rs
@@ -0,0 +1,63 @@
+use solana_idlgen::idlgen;
+
+idlgen!({
+    "version": "0.1.0",
+    "name": "Turbin3_prereq",
+    "instructions": [
+          {
+            "name": "complete",
+            "accounts": [
+              {
+                "name": "signer",
+                "isMut": true,
+                "isSigner": true
+              },
+              {
+                "name": "prereq",
+                "isMut": true,
+                "isSigner": false
+              },
+              {
+                "name": "systemProgram",
+                "isMut": false,
+                "isSigner": false
+              }
+            ],
+            "args": [
+              {
+                "name": "github",
+                "type": "bytes"
+              }
+            ]
+          },
+          {
+            "name": "update",
+            "accounts": [
+              {
+                "name": "signer",
+                "isMut": true,
+                "isSigner": true
+              },
+              {
+                "name": "prereq",
+                "isMut": true,
+                "isSigner": false
+              },
+              {
+                "name": "systemProgram",
+                "isMut": false,
+                "isSigner": false
+              }
+            ],
+            "args": [
+              {
+                "name": "github",
+                "type": "bytes"
+              }
+            ]
+          }
+    ],
+    "metadata": {
+        "address": "HC2oqz2p6DEWfrahenqdq2moUcga9c9biqRBcdK3XKU1"
+    }
+});
